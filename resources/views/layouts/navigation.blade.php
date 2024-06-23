603d584394ae5333ab97f06a022a3ff1e6e82b1f
Martin <22581506+dis-lekker-by-die-see@users.noreply.github.com>
2024-06-23 21:22:47 +0900

    Add x-responsive-nav-links to navigation.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -119,7 +119,7 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
+    {{-- <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
@@ -151,5 +151,64 @@
                 </form>
             </div>
         </div>
+    </div> --}}
+    <!-- Responsive Navigation Menu -->
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <!-- Nav links based on user role -->
+            @if (Auth::check())
+                @if (in_array(Auth::user()->role, ['rider', 'club', 'organizer', 'kami']))
+                    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('temp- Program') }}
+                    </x-responsive-nav-link>
+                @endif
+                @if (in_array(Auth::user()->role, ['club', 'organizer', 'kami']))
+                    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('temp- Schedule') }}
+                    </x-responsive-nav-link>
+                @endif
+                @if (in_array(Auth::user()->role, ['organizer', 'kami']))
+                    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Competitions') }}
+                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Events') }}
+                    </x-responsive-nav-link>
+                @endif
+                @if (Auth::user()->role === 'kami')
+                    <x-responsive-nav-link :href="route('query')" :active="request()->routeIs('query')">
+                        {{ __('Query') }}
+                    </x-responsive-nav-link>
+                @endif
+            @endif
+        </div>
+
+        <!-- Responsive Settings Options -->
+        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+            <div class="px-4">
+                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+            </div>
+
+            <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('profile.edit')">
+                    {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-responsive-nav-link>
+                </form>
+            </div>
+        </div>
     </div>
+
 </nav>
