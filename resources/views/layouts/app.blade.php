02f2372af32904020d7e6027173895189b85b91d
Martin <22581506+dis-lekker-by-die-see@users.noreply.github.com>
2024-06-25 23:34:01 +0900

    Vite is fine for now. Try to fix favicon tabl title problem.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', '日本馬術競技大会') }}</title>
+        {{-- <title>@yield('title', config('app.name', '日本馬術競技大会'))</title> --}}
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>日本馬術競技大会</title>
+        {{-- <title>日本馬術競技大会</title> --}}
+        <title>{{ config('app.name', '日本馬術競技大会') }}</title>
+
+        
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
