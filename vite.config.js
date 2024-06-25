60a194a09562be3024054139a83092a37264e0b9
Martin <22581506+dis-lekker-by-die-see@users.noreply.github.com>
2024-06-25 21:46:50 +0900

    Same as before

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,30 @@
 //     ],
 // });
 
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css',
+//                 'resources/js/app.js',
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+//     server: {
+//         watch: {
+//             usePolling: true,
+//             interval: 300,
+//         },
+//         host: '0.0.0.0',
+//         port: 5173,
+//     },
+// });
+
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -33,5 +57,9 @@ export default defineConfig({
         },
         host: '0.0.0.0',
         port: 5173,
+        hmr: {
+            host: '13.208.182.103', // Replace with your EC2 public IP or domain
+            port: 5173,
+        },
     },
 });
