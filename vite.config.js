34713177e395901e7fe7a60574421d86514f1865
Martin <22581506+dis-lekker-by-die-see@users.noreply.github.com>
2024-06-25 21:01:46 +0900

    One more attempt at fixing vite.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css',
+//                 'resources/js/app.js',
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -11,4 +26,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        watch: {
+            usePolling: true,
+            interval: 300,
+        },
+        host: '0.0.0.0',
+        port: 5173,
+    },
 });
