cbcc7aeaf4db90137652d5fb6f1a2701eb7a0ae8
Martin <22581506+dis-lekker-by-die-see@users.noreply.github.com>
2024-06-26 00:02:22 +0900

    Same as before.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', '日本馬術競技大会') }}</title>
-        {{-- <title>@yield('title', config('app.name', '日本馬術競技大会'))</title> --}}
+        {{-- <title>{{ config('app.name', '日本馬術競技大会') }}</title> --}}
+        <title>@yield('title', config('app.name', '日本馬術競技大会'))</title>
 
 
         <!-- Fonts -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         {{-- <title>日本馬術競技大会</title> --}}
-        <title>{{ config('app.name', '日本馬術競技大会') }}</title>
+        {{-- <title>{{ config('app.name', '日本馬術競技大会') }}</title> --}}
 
-        
+        @section('title', 'Welcome to My Application')
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
