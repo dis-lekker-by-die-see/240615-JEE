4209a76fe3cd70285c058951c0ec4fa1267e0e11
Martin <22581506+dis-lekker-by-die-see@users.noreply.github.com>
2024-07-06 15:36:26 +0900

    Clean up code.

--- routes/web.php
+++ routes/web.php
@@ -159,15 +159,11 @@ Route::middleware(['auth'])->group(function () {
 
 Route::middleware(['auth', 'role:club'])->group(function () {
     Route::get('/club/dashboard', [ClubController::class, 'dashboard'])->name('club.dashboard');
-    Route::get('/club/details', [ClubController::class, 'show'])->name('club.details'); // New route for club details
-    Route::post('/club/details', [ClubController::class, 'store'])->name('club.store'); // New route for storing club details
+    Route::get('/club/details', [ClubController::class, 'show'])->name('club.details'); // Route for club details
+    Route::post('/club/details', [ClubController::class, 'store'])->name('club.store'); // Route for storing club details
     Route::get('/club/riders', [ClubController::class, 'clubRiders'])->name('club.riders');
-
-    //Route::post('/club/riders', [ClubController::class, 'storeRider'])->name('club.storeRider'); // New route for storing rider
-    Route::post('/club/storeRider', [ClubController::class, 'storeRider'])->name('club.storeRider'); // New route for storing new rider details
+    Route::post('/club/storeRider', [ClubController::class, 'storeRider'])->name('club.storeRider'); // Route for storing new rider details
     Route::delete('/club/riders/{rider_id}', [ClubController::class, 'deleteRider'])->name('club.deleteRider');
-
-
     Route::post('/club/approve/{rider_id}', [ClubController::class, 'approveRider'])->name('club.approveRider');
     Route::post('/club/decline/{rider_id}', [ClubController::class, 'declineRider'])->name('club.declineRider');
 });
